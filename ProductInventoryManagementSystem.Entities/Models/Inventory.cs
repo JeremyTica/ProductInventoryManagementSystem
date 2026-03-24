223611a41b58dcb1f3d08e8557deb2cd756cb9c9
JeremyTica <jeremytica@gmail.com>
2026-03-24 08:50:19 -0400

    Created model entities Inventory and Product, which will be used to represent a template for any product to be assigned any (or appropriate) inventory.

---
+++ ProductInventoryManagementSystem.Entities/Models/Inventory.cs
@@ -0,0 +1,20 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace ProductInventoryManagementSystem.Entities.Models
+{
+    /// <summary>
+    /// This class represents an inventory as a model, which will be used to manage the stock of products.
+    /// </summary>
+    public class Inventory
+    {
+        public int Id { get; set; }
+        public int ProductId { get; set; }
+        public Product? Product { get; set; }
+        public int QuantityInStock { get; set; }
+        public int ReorderLevel { get; set; }
+    }
+}
---
+++ ProductInventoryManagementSystem.Entities/Models/Product.cs
@@ -0,0 +1,23 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace ProductInventoryManagementSystem.Entities.Models
+{
+    /// <summary>
+    /// This class represents a product as a model which will be a part of an inventory management system.
+    /// </summary>
+    public class Product
+    {
+        public int Id { get; set; }
+        public string? Name { get; set; }
+        public string? Description { get; set; }
+        public decimal Price { get; set; }
+        public string? Category { get; set; }
+        public string? SKU { get; set; }
+        public DateTime CreatedDate { get; set; } = DateTime.Now;
+        public Inventory? Inventory { get; set; }
+    }
+}
