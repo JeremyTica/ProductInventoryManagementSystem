d83dec1a8485bf2178ca52308fb66fba164f2de9
JeremyTica <jeremytica@gmail.com>
2026-03-24 09:55:19 -0400

    Created a new repository dedicated from managing the data access for Product items.
    This repository will be abstracted (Inversion of Control) by an interface which will be serviced and injected later to perform various operations. The repository intends to provide various CRUD operations
    
    The first operation method AddProduct was added to create a new product to be stored and saved into the database.

---
+++ ProductInventoryManagementSystem.Services/ProductServices/ProductRepository.cs
@@ -0,0 +1,43 @@
+﻿using ProductInventoryManagementSystem.Entities.Models;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace ProductInventoryManagementSystem.Services.ProductServices
+{
+    /// <summary>
+    /// This class represents a repository that will be used to manage data access for products within the management system
+    /// </summary>
+    public class ProductRepository
+    {
+        ProductDbContext _productDbContext;
+
+        /// <summary>
+        /// This constructor is used to inject the database context into the repository, allowing it to perform data access operations on the product data.
+        /// </summary>
+        /// <param name="productDbContext">
+        /// The database context to be used by the repository
+        /// </param>
+        public ProductRepository(ProductDbContext productDbContext) {
+            _productDbContext = productDbContext;
+        }
+
+        /// <summary>
+        /// This function is responsible for adding a new product to the database
+        /// </summary>
+        /// <param name="product">
+        /// The product to be added
+        /// </param>
+        /// <returns>
+        /// The product after it was added to the database
+        /// </returns>
+        public Product AddProduct(Product product)
+        {
+            _productDbContext.Products.Add(product);
+            _productDbContext.SaveChanges();
+            return product;
+        }
+    }
+}
