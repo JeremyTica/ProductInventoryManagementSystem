06df6b89d00574c40ca31f7be4ac8eac137e7426
JeremyTica <jeremytica@gmail.com>
2026-03-24 12:47:28 -0400

    Created ProductController and IProductServices classes
    The product controller will be used to implement the API handlers for each product-related operation.
    
    It takes the IProductServices interface, which is the abstraction (layer) of the repository used to perform data access/business logic.

---
+++ ProductInventoryManagementSystem.Operations/Controllers/ProductController.cs
@@ -0,0 +1,27 @@
+﻿using Microsoft.AspNetCore.Http;
+using Microsoft.AspNetCore.Mvc;
+using ProductInventoryManagementSystem.Services.Interfaces;
+
+namespace ProductInventoryManagementSystem.Operations.Controllers
+{
+    /// <summary>
+    /// This class is the controller that will handle the API requests related to products in the management system
+    /// </summary>
+    [Route("api/[controller]")]
+    [ApiController]
+    public class ProductController : ControllerBase
+    {
+        IProductServices _productServices;
+
+        /// <summary>
+        /// This is the constructor used to inject the product services into the controller
+        /// </summary>
+        /// <param name="productServices">
+        /// The product services to be used by the controller for handling product-related operations
+        /// </param>
+        public ProductController(IProductServices productServices)
+        {
+            _productServices = productServices;
+        }
+    }
+}
---
+++ ProductInventoryManagementSystem.Services/Interfaces/IProductServices.cs
@@ -0,0 +1,22 @@
+﻿using ProductInventoryManagementSystem.Entities.Models;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace ProductInventoryManagementSystem.Services.Interfaces
+{
+    /// <summary>
+    /// This interface is this abstraction of the product services, which will be implemented by the ProductService class to provide the necessary functionalities for managing products within the inventory management system
+    /// </summary>
+    public interface IProductServices
+    {
+        IEnumerable<Product> GetAllProducts();
+        Product GetProduct(int id);
+        Product AddProduct(Product product);
+        Product UpdateProduct(Product product);
+        void DeleteProduct(int id);
+
+    }
+}
