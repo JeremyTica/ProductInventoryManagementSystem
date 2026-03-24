e47b341eeef351f1782754c57a91574ae95598ea
JeremyTica <jeremytica@gmail.com>
2026-03-24 09:10:43 -0400

    Created the database context class within the Services project as a means enable ORM functionality and database connections for data access

---
+++ ProductInventoryManagementSystem.Services/ProductDbContext.cs
@@ -0,0 +1,28 @@
+﻿using Microsoft.EntityFrameworkCore;
+using ProductInventoryManagementSystem.Entities.Models;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace ProductInventoryManagementSystem.Services
+{
+    /// <summary>
+    /// This class is the database context that will be used by Entity Framework Core to map the corresponding models to database tables
+    /// </summary>
+    public class ProductDbContext : DbContext
+    {
+        /// <summary>
+        /// This constructor initializes the database context using the specific options
+        /// </summary>
+        /// <param name="options"></param>
+        public ProductDbContext(DbContextOptions<ProductDbContext> options)
+            : base(options)
+        {
+        }
+
+        public DbSet<Product> Products { get; set; }
+        public DbSet<Inventory> Inventories { get; set; }
+    }
+}
